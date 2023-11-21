00db767d74934672f7e7530e84853c38e4af11c3
jchav1996 <jenniferschavarria@gmail.com>
2023-11-20 20:55:20 -0500

    changes

--- src/components/navigation/about/About.jsx
+++ src/components/navigation/about/About.jsx
@@ -4,6 +4,11 @@ export function ABOUT () {
     return (
         
 <div className='about'>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
 <h1>JSC Transport is commited to making your Logistic needs easy and simple</h1>
 </div>
 );
--- src/components/navigation/contact/Contact.jsx
+++ src/components/navigation/contact/Contact.jsx
@@ -2,6 +2,11 @@
 export function CONTACT() {
     return (
         <div className="container">
+                <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
 <h1>Hello Contact</h1>
         </div>
     );
