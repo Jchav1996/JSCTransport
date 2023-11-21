e390653e36f49ddd79f0a6f64ad777cbcf8694ec
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-11-20 20:19:38 -0500

    removed google font link from pages

--- src/components/navigation/hero/Hero.jsx
+++ src/components/navigation/hero/Hero.jsx
@@ -1,9 +1,6 @@
 export function Hero() {
     return (
         <div className="container">
-            <style>
-  @import url('https://fonts.googleapis.com/css2?family=Inter&family=Libre+Baskerville:ital@1&family=Merriweather&family=Mooli&family=Mulish:wght@300&family=Pridi&family=Roboto:wght@300&family=Signika:wght@300&display=swap');
-</style>
 <img src="src\images\JSCtruck.jpg"/>
         </div>
     );
