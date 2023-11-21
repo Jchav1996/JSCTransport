42697c52d8e92e3816bb0e7671b15868b26790d1
jchav1996 <jenniferschavarria@gmail.com>
2023-11-20 20:14:39 -0500

    chages

---
+++ src/components/navigation/quote/Quote.jsx
@@ -0,0 +1,13 @@
+import "./quote.css"
+
+export function Quote () {
+    return (
+        
+<div className='quote'>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Inter&family=Libre+Baskerville:ital@1&family=Merriweather&family=Mooli&family=Mulish:wght@300&family=Pridi&family=Roboto:wght@300&family=Signika:wght@300&display=swap');
+</style>
+<h1>Hello Quote</h1>
+</div>
+);
+}
