8a09860203572f7562dd79270e3a45b590be0abe
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-11-22 20:14:06 -0500

    sync

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -6,30 +7,43 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ABOUT } from './components/navigation/about/About.jsx';
 import { Navbar } from './components/navigation/Navbar.jsx';
 import { Hero } from './components/navigation/hero/Hero.jsx';
+import { Users, loader as usersLoader } from "./components/Users.jsx";
+import User, { loader } from "./components/Users.jsx";
+
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Navbar/>,
-    children:[
+    path: "/",
+    element: <Navbar />,
+    children: [
       {
         path: "/home",
-        element: <Hero /> 
-    },
+        element: <Hero />
+      },
+      {
+        path: "/about",
+        element: <ABOUT />
+      }
+    ]
+  },
   {
-    path: "/about",
-    element: <ABOUT />
-  }
+    path: "/users",
+    element: <Users />
+    loader: usersLoader,
+  },
 
-]
-    
-  }
+  {
+    path: "user/:email",
+    element: <User />
+    loader: Loader,
+  },
+
 ])
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
