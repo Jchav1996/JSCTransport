965d64192bb85fd707995b2bf1df2fb4f68d7e3c
Mr. Mendez <mrmendez@amvestor.com>
2023-12-02 15:22:31 -0500

    Refactor code and update dependencies

--- src/components/HeroSlideShow/HeroSlideShow.jsx
+++ src/components/HeroSlideShow/HeroSlideShow.jsx
@@ -1,50 +1,59 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./HeroSlideShow.css";
 
-import slide1 from "../img/truck3.jpg"
-import slide2 from "../img/truck1.jpg"
-import slide3 from "../img/truck2.jpg"
+import slide1 from "../img/truck3.jpg";
+import slide2 from "../img/truck1.jpg";
+import slide3 from "../img/truck2.jpg";
 
 const HeroSlideShow = () => {
-  const slides = [
-    { image: slide1 },
-    { image: slide2 },
-    { image: slide3 },
-  ];
+	const slides = [{ image: slide1 }, { image: slide2 }, { image: slide3 }];
 
-  const [currentSlide, setCurrentSlide] = useState(0);
+	const [currentSlide, setCurrentSlide] = useState(0);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentSlide((prevCurrentSlide) => (prevCurrentSlide + 1) % slides.length);
-    }, 15000);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCurrentSlide(
+				(prevCurrentSlide) => (prevCurrentSlide + 1) % slides.length
+			);
+		}, 15000);
 
-    return () => clearInterval(timer);
-  }, [slides.length]);
-  const prevSlide = () => {
-    setCurrentSlide(prevCurrentSlide => (prevCurrentSlide - 1 + slides.length) % slides.length);
-  };
+		return () => clearInterval(timer);
+	}, [slides.length]);
+	const prevSlide = () => {
+		setCurrentSlide(
+			(prevCurrentSlide) =>
+				(prevCurrentSlide - 1 + slides.length) % slides.length
+		);
+	};
 
-  const nextSlide = () => {
-    setCurrentSlide(prevCurrentSlide => (prevCurrentSlide + 1 + slides.length) % slides.length);
-  };
+	const nextSlide = () => {
+		setCurrentSlide(
+			(prevCurrentSlide) =>
+				(prevCurrentSlide + 1 + slides.length) % slides.length
+		);
+	};
 
-  return (
-    <div className="hero-slide-show">
-      <button onClick={prevSlide}>Previous</button>
-      {slides.map((slide, index) => (
-        <div key={index} className={`slide ${index === currentSlide ? 'active' : ''}`}>
-          <img id='heroImg' src={slide.image} alt={slide.description} />
-          <p>{slide.description}</p>
-        </div>
-      ))}
-      <button onClick={nextSlide}>Next</button>
-    </div>
-  );
+	return (
+		<div className="hero-slide-show">
+			<button onClick={prevSlide}>Previous</button>
+			{slides.map((slide, index) => (
+				<div
+					key={index}
+					className={`slide ${
+						index === currentSlide ? "active" : ""
+					}`}>
+					<img
+						id="heroImg"
+						src={slide.image}
+						alt={slide.description}
+					/>
+					<p>{slide.description}</p>
+				</div>
+			))}
+			<button onClick={nextSlide}>Next</button>
+		</div>
+	);
 };
 
 export default HeroSlideShow;
-
-
-
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,18 +1,18 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export function Navbar() {
-  return (
-    <>
-      <nav>
-        <div className="navbar">
-          <h2> JSC Transport Enterprises Corp.</h2>
-          <Link to="/">Home</Link>
-          <Link to="contact">Contact</Link>
-          <Link to="about">About Us</Link>
-          <Link to="quote">Quote</Link>
-        </div>
-      </nav>
-    </>
-  );
+	return (
+		<>
+			<nav>
+				<div className="navbar">
+					<h2> JSC Transport Enterprises Corp.</h2>
+					<Link to="/">Home</Link>
+					<Link to="contact">Contact</Link>
+					<a href="#about">About Us</a>
+					<Link to="quote">Quote</Link>
+				</div>
+			</nav>
+		</>
+	);
 }
