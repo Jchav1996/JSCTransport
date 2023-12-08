ffed09cfa1836b962b1387a3b0adf8eb72ed0ad2
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-12-08 18:12:56 -0500

    quote form

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -1,11 +1,35 @@
-import "./quote.css";
+import { useState } from 'react';
+import './quote.css';
 
-export function Quote () {
+export function Quote() {
+    // eslint-disable-next-line no-unused-vars
+    const [count, setCount] = useState(0);
+
     return (
-        
-<div className="quote">
-
-<h1>Hello Quote</h1>
-</div>
-);
+        <div>
+            <form><label>*Full name :</label>
+                <label></label>
+                <input type="text" required={true} />
+                <br />Company :<input type="text" />
+                <br /><label>*Phone Number :<input type="number" required={true} />
+                </label>
+                <br />
+                <label>*Your Email :</label><input type="text" required={true} /><br />
+                <label>*Pick-Up Origin :<input required={true} />
+                    <br />
+                    <label>*Drop Off Destination :<input required={true} />
+                    </label>
+                    <br />
+                    <label>*Total Weight - (Ibs) :<input type="number" required={true} /></label>
+                    <label>
+                        <br />Additional Notes :</label>
+                    <br />
+                    <textarea maxLength={800} />
+                    <br />
+                </label>
+                <input type="submit" value="Submit" /></form>
+    </div>
+    );
 }
+
+
