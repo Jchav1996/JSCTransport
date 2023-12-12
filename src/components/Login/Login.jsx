d7fb2adfc35746e6b309b60e810415245259ebd8
Mr. Mendez <mrmendez@amvestor.com>
2023-12-12 10:14:14 -0500

    auth working

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,55 +1,56 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
 import { signInUser } from "../../lib/firebase";
 import { useNavigate } from "react-router-dom";
 
 export function Login() {
-const [formFields, setFormFields] = useState({
-    email: "",
-    password: "",
-});
+	const [formFields, setFormFields] = useState({
+		email: "",
+		password: "",
+	});
 
-const { email, password } = formFields;
+	const { email, password } = formFields;
 
-const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormFields((prev) => ({...prev, [name]: value }));
-};
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormFields((prev) => ({ ...prev, [name]: value }));
+	};
 
-useEffect(() => {
-    console.log(formFields);
-}, [formFields]);
+	const navigate = useNavigate();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const userCredentials = await signInUser(email, password);
+			if (userCredentials) {
+				setFormFields({ email: "", password: "" });
 
-const navigate = useNavigate();
-const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-        const userCredentials = await signInUser(email, password);
-        if (userCredentials) {
-            setFormFields({ email: "", password: ""});
+				navigate("/private");
+			}
+		} catch (error) {
+			console.log("Error signing in user: ", error.message);
+		}
+	};
 
-            navigate("/private");
-        }
-
-    } catch (error) {
-        console.log("Error signing in user: ", error.message)
-    }
+	return (
+		<form onSubmit={handleSubmit}>
+			<label htmlFor="email">Username:</label>
+			<input
+				type="email"
+				name="email"
+				value={email}
+				onChange={handleChange}
+			/>
+			<br></br>
+			<label htmlFor="password">Password:</label>
+			<input
+				type="password"
+				name="password"
+				value={password}
+				onChange={handleChange}
+			/>
+			<br></br>
+			<button id="sub" type="submit">
+				Submit
+			</button>
+		</form>
+	);
 }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input type="email" name="email" value={email} onChange={handleChange} />
-            <br></br>
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" value={password} onChange={handleChange} />
-            <br></br>
-            <button id="sub" type="submit">Submit</button>
-        </form>
-    )
-}
-
-// const resetFormFields = () => {
-// };
-
-// const handleSubmit = async (event) => {
-// };
