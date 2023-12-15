749c890b9fe442535d340b44bc2c0c7d90ddd809
jchav1996 <jenniferschavarria@gmail.com>
2023-12-15 15:27:55 -0500

    officially done

--- src/components/private/Dashboard.jsx
+++ src/components/private/Dashboard.jsx
@@ -1,24 +1,12 @@
 import { useState } from 'react'
 
 export function Dashboard() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='dashtest'>
+        <h1>
+          SHIPMENT TRACKER IS UNDER CONSTRUCTION 
+      </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          HELLLLLEERRRR ! Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        HELLLLEEERRRRR Click on the Vite and React logos to learn more
-      </p>
-    </>
   );
 }
